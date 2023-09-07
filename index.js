ca86f234eb1681abf1afc1929e2d1e945dd3d545
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2023-09-07 17:14:31 +0100

    updated

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const time = document.getElementById('time')
 
 let today
 const new_date = new Date()
-const current_date = new_date.getUTCDay()
-const hours = new_date.getUTCHours()
-const mins = new_date.getUTCMinutes()
+let current_date = new_date.getUTCDay()
+let hours = new_date.getUTCHours()
+let mins = new_date.getUTCMinutes()
 if (mins < 10) {
   mins = `0${mins}`
+}
+if (hours < 10) {
+  hours = `0${hours}`
 }
 const seconds = new_date.getUTCSeconds()
 
